fa74d91f823a540d0699e1eee7286fa9a82ef5ef
Miruthulkumar <127958188+Miruthulkumar@users.noreply.github.com>
2023-05-06 16:01:48 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,73 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+firstoperand=int(input("Enter first number: "))
+
+Enter first number: 100
+
+secondoperand=int(input("Enter second number: "))
+
+Enter second number: 2
+
+operator=input("Enter your preferred operatot")
+
+Enter your preferred operatot
+
+operator=input("Enter your preferred operator ")
+
+Enter your preferred operator +
+
+if '+'==operator:
+    a=firstoperand+secondoperand
+    print(a)
+elif('-'==operator:)
+SyntaxError: invalid syntax
+if '+'==operator:
+    a=firstoperand+secondoperand
+    print(a)
+elif('-'==operator):
+    b=firstoperand-secondoperand
+    print(b)
+elif('*'==operator):
+...     c=firstoperand*secondoperand
+...     print(c)
+... elif('/'==operator):
+...     d=firstoperand/secondoperand
+...     print(d)
+... else:
+...     print("Operator is not defined")
+... 
+...     
+102
+>>> 
+>>> 
+>>> firstoperand=int(input("Enter first number: "))
+... 
+Enter first number: 200
+>>> 
+>>> secondoperand=int(input("Enter second number: "))
+... 
+Enter second number: 40
+>>> 
+>>> operator=input("Enter your preferred operator ")
+... 
+Enter your preferred operator /
+>>> 
+>>> if '+'==operator:
+...     a=firstoperand+secondoperand
+...     print(a)
+... elif('-'==operator):
+...     b=firstoperand-secondoperand
+...     print(b)
+... elif('*'==operator):
+...     c=firstoperand*secondoperand
+...     print(c)
+... elif('/'==operator):
+    d=firstoperand/secondoperand
+    print(d)
+else:
+    print("Operator is not defined")
+
+    
+
+5.0
+
