e8c646a253e564678e3f27b30fc3680733cb458d
Miruthulkumar <127958188+Miruthulkumar@users.noreply.github.com>
2023-05-06 16:00:31 +0530

    Add files via upload

---
+++ bb performance calculator.py
@@ -0,0 +1,95 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+point=int(input("Enter the number of points scored by you: "))
+Enter the number of points scored by you: 32
+
+if point<=10:
+    print("poor performance")
+elif(point>=10 and point<=20):
+    print("Decent performance")
+elif(point>=21 and point<=30):
+    print("Good performance")
+elif(point>31 and point<=40):
+    print("Very good performance")
+
+    
+Very good performance
+point=int(input("Enter the number of points scored by you: "))
+
+Enter the number of points scored by you: 9
+
+
+if point<=10:
+    print("poor performance")
+elif(point>=10 and point<=20):
+    print("Decent performance")
+elif(point>=21 and point<=30):
+    print("Good performance")
+elif(point>31 and point<=40):
+    print("Very good performance")
+
+    
+poor performance
+
+point=int(input("Enter the number of points scored by you: "))
+
+Enter the number of points scored by you: 23
+
+
+if point<=10:
+    print("poor performance")
+elif(point>=10 and point<=20):
+    print("Decent performance")
+elif(point>=21 and point<=30):
+    print("Good performance")
+elif(point>31 and point<=40):
+    print("Very good performance")
+
+Good performance
+
+point=int(input("Enter the number of points scored by you: "))
+
+Enter the number of points scored by you: 14
+
+if point<=10:
+    print("poor performance")
+elif(point>=10 and point<=20):
+    print("Decent performance")
+elif(point>=21 and point<=30):
+    print("Good performance")
+elif(point>31 and point<=40):
+    print("Very good performance")
+
+...     
+Decent performance
+>>> 
+>>> point=int(input("Enter the number of points scored by you: "))
+... 
+Enter the number of points scored by you: 100
+>>> 
+>>> if point<=10:
+...     print("poor performance")
+... elif(point>=10 and point<=20):
+...     print("Decent performance")
+... elif(point>=21 and point<=30):
+...     print("Good performance")
+... elif(point>31 and point<=40):
+...     print("Very good performance")
+... 
+...     
+>>> point=int(input("Enter the number of points scored by you: "))
+... 
+Enter the number of points scored by you: 32
+>>> 
+>>> if point<=10:
+...     print("poor performance")
+... elif(point>=10 and point<=20):
+...     print("Decent performance")
+... elif(point>=21 and point<=30):
+...     print("Good performance")
+... elif(point>31 and point<=40):
+...     print("Very good performance")
+... 
+...     
+Very good performance
+>>> 
